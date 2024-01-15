b29a8d4e24eab7dd422480c2c5e4ff87f62bc6d6
KhalidMimMuzahid <khalidmimm@gmail.com>
2024-01-15 15:53:26 +0600

    cpp input and loop

---
+++ input/array_of_number.cpp
@@ -0,0 +1,24 @@
+
+#include<bits/stdc++.h> //this header file import all header file for c and c++
+using namespace std;
+int main() {
+
+
+    int numArray[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>numArray[i];
+    }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<" "<<numArray[i];
+    }
+    cout<<endl;
+
+
+
+
+    return 0;
+}
